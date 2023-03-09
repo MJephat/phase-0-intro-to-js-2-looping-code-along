97abbe859d6f9b555f84e88d622e2ef7702c6360
MJephat <mukundijephat22@gmail.com>
2023-03-09 05:36:56 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const gifts = ["Ada", "Brendan", "Ali"];
+
+function writecards(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Thank you, ${gifts[i]} for the wornderful birthday gift!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+writecards(gifts);
+
+
+function numbers(){
+    let countDown=10;
+    while(countDown>=0){
+        console.log(countDown--)
+    }
+    return numbers;
+}
+numbers();
